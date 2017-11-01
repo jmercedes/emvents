1ed1b8a8e886dd1eb2b925354dfb7b868b0b7961
Juan <j.mercedes@thedesignpill.com>
2017-10-31 21:33:55 -0400

    add routes

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 // Initial file
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const server = express();
+const PORT = 3000;
+
+
+
+server.listen(PORT, () => {
+  console.log(`Server is runnig on ${PORT}`);
+})
+
+
+// New routes
+// Get
+server.get('/', (req, res) => {
+  res.send('this is a get request')
+})
+
+// Post
+server.post('/', (req, res) => {
+  res.send('this is a post request')
+})
